e069648e2035abe15e535913bd8cce5c3b2df56e
hadarco <34810525+hadarco@users.noreply.github.com>
2019-05-20 20:02:33 +0300

    Update 0002_auto_20161201_0142.py

--- src/accounts/migrations/0002_auto_20161201_0142.py
+++ src/accounts/migrations/0002_auto_20161201_0142.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.db import migrations, models
 
-גדדד
+
 class Migration(migrations.Migration):
 
     dependencies = [
