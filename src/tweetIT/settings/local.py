4f9b686f2fb223bfca22c87b814f76491d4934d3
GalitAb <34810522+GalitAb@users.noreply.github.com>
2019-05-21 16:46:22 +0300

    Update local.py

--- src/tweetIT/settings/local.py
+++ src/tweetIT/settings/local.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+import math
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # manage.py
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
