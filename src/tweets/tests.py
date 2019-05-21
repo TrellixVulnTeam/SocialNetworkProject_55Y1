d7a5952305c1a09ae73a9867965e2a47bf8dad9e
lior <liorrezn@gmail.com>
2019-05-21 19:03:48 +0300

    tests validation of tweet

--- src/tweets/tests.py
+++ src/tweets/tests.py
@@ -21,6 +21,14 @@ class TweetModelTestCase(TestCase):
             )
         self.assertTrue(obj.content == "test")
         self.assertTrue(obj.id == 1)
+
+    def test_tweet_item2(self):
+        obj = Tweet.objects.create(
+                user= User.objects.first(),
+                content='test'
+            )
+        self.assertTrue(obj.content == "test")
+        self.assertTrue(obj.id == 1)
         absolute_url = reverse("tweet:detail", kwargs={"pk": 1})
         self.assertTrue(obj.get_absolute_url() == absolute_url)
 
