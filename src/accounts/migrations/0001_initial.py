e5cd5a62c522f5adb068745b5a5ab4859841218d
Ran Avital <34808170+ranavital@users.noreply.github.com>
2019-05-21 18:12:08 +0300

    Update 0001_initial.py

--- src/accounts/migrations/0001_initial.py
+++ src/accounts/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
-
+     
     operations = [
         migrations.CreateModel(
             name='UserProfile',
