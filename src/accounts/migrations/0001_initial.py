c1139332157edd2ac451ed18a2e8735487b3b4f5
Ran Avital <34808170+ranavital@users.noreply.github.com>
2019-05-21 18:12:27 +0300

    Update 0001_initial.py

--- src/accounts/migrations/0001_initial.py
+++ src/accounts/migrations/0001_initial.py
@@ -10,7 +10,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     initial = True
-
+ 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
